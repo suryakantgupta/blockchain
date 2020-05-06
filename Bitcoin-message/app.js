6a96e72fc4b041be54ad0afe780163822ac513b3
suryakantgupta <suryakantgupta66@gmail.com>
2020-05-06 11:08:52 +0530

    t

--- Bitcoin-message/app.js
+++ Bitcoin-message/app.js
@@ -1,19 +1,13 @@
-const bitcoin =require('bitcoinjs-lib')
-const bitcoinMessage = require('bitcoinjs-message')
+const Client = require('bitcoin-core')
+var config = {
+    network: 'testnet',
+    username: 'surya',
+    password: 'suryakant',
+    port: '18332'
 
+};
 
-//var keyPair = bitcoin.ECPair.fromWIF('0380e13342de0e9a33bd9e8e5bb0f6cbd9acee68b79ea0a8e299aed920d5a19bc3')
-//var privateKey = keyPair.privateKey
+var rpc = new Client(config)
 
-
-
-
-const address = 'bc1qlzf28s47dtw3y4gzaxg9swj868p4n864arzddg'
-const message = 'Suryakant'
-const privateKey = 'p2wpkh:KwMNRZutJo1Ht4MzrExzK67LD4Ff17HmZGiab4AAR48etrXZYX3d'
-
-var signature = bitcoinMessage.sign(message,privateKey,address)
-
-console.log(signature.toString('base64'))
-
-//console.log(bitcoinMessage.verify(message,address,signature));
+rpc.listUnspent().then((help)=>console.log(help))
+//console.log(rpc.listunspent())
